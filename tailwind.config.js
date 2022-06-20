31071a90acb23519004bef72265a217056ecd46a
DoraFun <lolovichlol606@gmail.com>
2022-06-21 02:09:08 +0500

    321

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,16 +12,24 @@ module.exports = {
         'menu-color': '#383A49',
         'Numbers': '#109FCB',
         'LighGray': "#797E9D",
+        'Button-Gray': "#797E9D",
+        'BorderGray': "#D6D6D6",
+        'link': "#109FCB",
+        'Dark-gray': "#666666",
         
       },
       boxShadow: {
         'buttonShadow': "0px 7px 13px rgba(255, 53, 60, 0.25)",
         'textShadow': "0px 4px 20px rgba(0, 0, 0, 0.2)",
+        'buttonShadow-2': "0px 7px 13px rgba(0, 0, 0, 0.25)"
       },
       gridTemplateColumns: {
         'contentSize-2': "repeat(2, max-content)",
         'contentSize-4': "repeat(4, max-content)",
         'contentSize-5': "repeat(5, max-content)",
+      },
+      gridTemplateRows: {
+        'contentSize-3': "repeat(3, max-content)",
       },
       borderColor: {
         'menuBorder': '#E9EEF1',
