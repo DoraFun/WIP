f42127b9bd82facc2e4efe0eea890a53313b3389
DoraFun <lolovichlol606@gmail.com>
2022-06-24 02:07:37 +0500

    Small screens ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,15 +21,21 @@ module.exports = {
       boxShadow: {
         'buttonShadow': "0px 7px 13px rgba(255, 53, 60, 0.25)",
         'textShadow': "0px 4px 20px rgba(0, 0, 0, 0.2)",
-        'buttonShadow-2': "0px 7px 13px rgba(0, 0, 0, 0.25)"
+        'buttonShadow-2': "0px 7px 13px rgba(0, 0, 0, 0.25)",
+        'button-shadow-3': "0px 3px 40px rgba(0, 0, 0, 0.11)"
       },
       gridTemplateColumns: {
         'contentSize-2': "repeat(2, max-content)",
         'contentSize-4': "repeat(4, max-content)",
+        'contentSize-3': "repeat(3, max-content)",
         'contentSize-5': "repeat(5, max-content)",
+        'contentSize-full': "repeat(99, max-content)",
       },
       gridTemplateRows: {
         'contentSize-3': "repeat(3, max-content)",
+        'contentSize-2': "repeat(2, max-content)",
+        '8': "repeat(8, minmax(0,1fr)) ",
+        'contentSize-8': "repeat(8, max-content) ",
       },
       borderColor: {
         'menuBorder': '#E9EEF1',
