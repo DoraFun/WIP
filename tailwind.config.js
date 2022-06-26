01c5a3d1491dca30b3ab06ca4e3d3e9f8a5a5908
DoraFun <lolovichlol606@gmail.com>
2022-06-27 00:58:08 +0500

    hfisdhgfisdh

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    
+    screens: {
+      "why": "980px",
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+      
+    },
     extend: {
       backgroundImage: {
-        "Header": "url('../img/Header_bg.png')"
+        "Header": "url('../img/Header_bg.png')",
+        "big_header": "url('../img/Header_bg_big.png')"
+        
       },
       colors: {
         'button-color': "#FF353C",
@@ -29,6 +48,7 @@ module.exports = {
         'contentSize-4': "repeat(4, max-content)",
         'contentSize-3': "repeat(3, max-content)",
         'contentSize-5': "repeat(5, max-content)",
+        'contentSize-6': "repeat(6, max-content)",
         'contentSize-full': "repeat(99, max-content)",
       },
       gridTemplateRows: {
@@ -46,6 +66,8 @@ module.exports = {
       height: {
         'Card': "17.501rem"
       },
+      
+      
 
     
     },
